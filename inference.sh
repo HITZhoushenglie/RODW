d9408d3ae1f6492dda29281fac506a99cf841b47
77Hub <45974000+HITZhoushenglie@users.noreply.github.com>
2026-07-16 14:31:20 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -3,32 +3,11 @@
 # Author: Shenglie Zhou
 # Date: 2025-8-25 / 2026-1-27
 
-# 128 x 128
-# ori + rein + blip 
-# insp2p+rein+blip: /home/ubuntu/code/RobustWide-master/train_results/2026-03-23T06-11-38
-
-# 192 x 192
-# ori + rein + blip 
-# insp2p+dsg+rlo: /home/ubuntu/code/RobustWide-master/train_results/2026-04-11T13-04-06
-
-# 256 x 256 128 bits
-# insp2p+ori+rein: /home/ubuntu/code/RobustWide-master/train_results/2026-04-11T12-37-42
-
-# 256 x 256
-# ori + rein (llm + rlo)
-# insp2p+ori+rein: /home/ubuntu/data0/model/RODW/train_results/2025-08-26T01-07-32
-# openimage+ori+rein: /home/ubuntu/data0/model/RODW/train_results/2025-08-20T01-40-27
-
-# ori + rein (vlm + rlo)
-# insp2p+ori+rein: /home/ubuntu/data0/model/RODW/train_results/single_style/vlm+rlo/2026-02-14T23-20-56
-
-# ori + rein + blip 
+# model
 # insp2p+rein+blip: /home/ubuntu/data0/model/RODW/train_results/2025-06-07T00-35-50
-# openimage+rein+blip: /home/ubuntu/data0/model/RODW/train_results/2025-07-18T13-07-43
 
 # datasets
 # insp2p: /home/ubuntu/datasets/robust_wide/data/muti_edited_200
-# openiamge: /home/ubuntu/datasets/robust_wide/data/OpenImage/openimage_200
 # insp2p: /home/ubuntu/data0/datasets/instructpix2pix/data/instructpix2pix_1200
 
 # test prompt
